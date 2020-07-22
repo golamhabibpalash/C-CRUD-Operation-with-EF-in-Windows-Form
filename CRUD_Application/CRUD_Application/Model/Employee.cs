5f9209d3208761e2690a55450f4137e4aae53c84
GH Palash <golamhabibpalash@gmail.com>
2020-07-22 11:24:23 +0600

    New Class name Employee added for model

---
+++ CRUD_Application/CRUD_Application/Model/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Application.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Dob { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
