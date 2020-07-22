f163e52cae7858cfa01ff07b0a136ba09e1c62d2
GH Palash <golamhabibpalash@gmail.com>
2020-07-22 11:39:41 +0600

    Migration added for New Class & update Database

---
+++ CRUD_Application/CRUD_Application/Migrations/202007220536143_EmployeeClassAdded.cs
@@ -0,0 +1,30 @@
+﻿namespace CRUD_Application.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeClassAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        EmpId = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 50),
+                        Email = c.String(maxLength: 50),
+                        Dob = c.String(maxLength: 50),
+                        Address = c.String(maxLength: 255),
+                        ImageURL = c.String(),
+                    })
+                .PrimaryKey(t => t.EmpId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
